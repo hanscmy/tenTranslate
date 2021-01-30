e763a546fefe295fd8f4a96fa30076a5464e3532
root <root@linux-test.asia-east2-a.c.myvpn-260414.internal>
2021-01-30 13:39:44 +0000

    支持python3

--- song.py
+++ song.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 
 import json
 import _thread
